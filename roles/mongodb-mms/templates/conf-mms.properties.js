8c10e8e70cdd02430d2819294278c0b895119db6
Gabriel Larios <gabriel.larios@AFSWWCLRQ0N4U9.us.accenturefederal.com>
2023-04-03 17:24:24 -0500

    added task and vars to template

--- roles/mongodb-mms/templates/conf-mms.properties.js
+++ roles/mongodb-mms/templates/conf-mms.properties.js
@@ -9,7 +9,7 @@ mms.mail.port=25
 mms.mail.ssl=false
 mms.mail.transport=smtp
 mms.publicApi.whitelistEnabled=true
-mongo.mongoUri=mongodb://10aab8140d6ebc074229195b3971b297f634d771e7517e271a2d2d27508ea314-d9193cfa83c88beaacc24f18a28ffc1e-3ba612aa58eafc917facad0ee794bc95:10aab8140d6ebc074229195b3971b297f634d771e7517e271a2d2d27508ea314-084bb1dbba7ddf6a7d9710d4428d8d8f-2aa2ea0057d2cf60a703341eb793d591@{{ ops_server_1 }}.opr.afsiep.net:37019{% if ops_server_2 is defined and ops_server_2|length %},{{ ops_server_2 }}.opr.afsiep.net:37019{% elif ops_server_3 is defined and ops_server_3|length %},{{ ops_server_3 }}.opr.afsiep.net:37019{% endif %}/?maxPoolSize=150{% if ops_server_2|length or ops_server_3|length %}&replicaSet=appdbRS{% endif %}
+mongo.mongoUri=mongodb://{{ mongo_username }}:{{ mongo_password }}@{{ ops_server_1 }}.opr.afsiep.net:37019{% if ops_server_2 is defined and ops_server_2|length %},{{ ops_server_2 }}.opr.afsiep.net:37019{% elif ops_server_3 is defined and ops_server_3|length %},{{ ops_server_3 }}.opr.afsiep.net:37019{% endif %}/?maxPoolSize=150{% if ops_server_2|length or ops_server_3|length %}&replicaSet=appdbRS{% endif %}
 mongo.encryptedCredentials=true
 automation.versions.source=local
 mongo.ssl=true
