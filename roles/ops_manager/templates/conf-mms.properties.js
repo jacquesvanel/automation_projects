cbcdd9fc54247ac830ec3a0de0bf30dd8b8c8c3e
Gabriel Larios <gabriel.larios@AFSWWCLRQ0N4U9.us.accenturefederal.com>
2023-04-17 16:55:08 -0500

    cleaned up roles

--- roles/ops_manager/templates/conf-mms.properties.js
+++ roles/ops_manager/templates/conf-mms.properties.js
@@ -9,7 +9,7 @@ mms.mail.port=25
 mms.mail.ssl=false
 mms.mail.transport=smtp
 mms.publicApi.whitelistEnabled=true
-mongo.mongoUri=mongodb://{{ mongo_username }}:{{ mongo_password }}@{{ ops_server_1 }}.opr.afsiep.net:37019{% if ops_server_2 is defined and ops_server_2|length %},{{ ops_server_2 }}.opr.afsiep.net:37019{% elif ops_server_3 is defined and ops_server_3|length %},{{ ops_server_3 }}.opr.afsiep.net:37019{% endif %}/?maxPoolSize=150{% if ops_server_2|length or ops_server_3|length %}&replicaSet=appdbRS{% endif %}
+mongo.mongoUri=mongodb://{{ mongo_username }}:{{ mongo_password }}@{{ ops_server_1 }}.opr.afsiep.net:37019{% if ops_server_2 is defined and ops_server_2|length %},{{ ops_server_2 }}.opr.afsiep.net:37019{% elif ops_server_3 is defined and ops_server_3|length %},{{ ops_server_3 }}.opr.afsiep.net:37019{% endif %}/?maxPoolSize=150{% if ops_server_2|length or ops_server_3|length %}&replicaSet=appdbRS{% endif %}  
 mongo.encryptedCredentials=true
 automation.versions.source=local
 mongo.ssl=false
